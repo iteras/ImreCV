68bb2b312ddb61f6eadd6a0037a9be7917784efb
Immu . <teras_8@hotmail.com>
2016-05-01 18:24:12 +0300

    DV as its self is ready, tables all shown, everything seems to work, no errors recognised, UI a bit updated

--- ImreCV/Controllers/HomeController.cs
+++ ImreCV/Controllers/HomeController.cs
@@ -3,16 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DAL;
+using DAL.Interfaces;
+using DAL.Repositories;
+using ImreCV.ViewModels;
 
 namespace ImreCV.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPersonRepository _personRepository = new PersonRepository(new DataBaseContext());
+        private readonly IComputerSkillRepository _computerSkill = new ComputerSkillRepository(new DataBaseContext());
+        private readonly IEducationRepository _educationRepository = new EducationRepository(new DataBaseContext());
+        private readonly IExpectationRepository _expectationRepository = new ExpectationRepository(new DataBaseContext());
+        private readonly IJobRepository _jobRepository = new JobRepository(new DataBaseContext());
+        private readonly ILanguageRepository _languageRepository = new LanguageRepository(new DataBaseContext());
+        private readonly IPersonAttributeRepository _personAttributeRepository = new PersonAttributeRepository(new DataBaseContext());
+
+
+
+
         public ActionResult Index()
         {
             return View();
         }
 
+        public ActionResult CVModel()
+        {
+            CVModel cvModel = new CVModel();
+            cvModel.AllPersons = _personRepository.All;
+            cvModel.ComputerSkills = _computerSkill.All;
+            cvModel.Educations = _educationRepository.All;
+            cvModel.Expectations = _expectationRepository.All;
+            cvModel.Jobs = _jobRepository.All;
+            cvModel.Languages = _languageRepository.All;
+            cvModel.PersonAttributes = _personAttributeRepository.All;
+
+            return View(cvModel);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ ImreCV/ViewModels/CVModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace ImreCV.ViewModels
+{
+   public class CVModel
+    {
+        public List<Person>  AllPersons { get; set; }
+       public List<ComputerSkill> ComputerSkills { get; set; }
+       public List<Education> Educations { get; set; }
+       public List<Expectation> Expectations { get; set; }
+       public List<Job> Jobs { get; set; }
+       public List<Language> Languages  { get; set; }
+       public List<PersonAttribute> PersonAttributes { get; set; }
+    }
+}
